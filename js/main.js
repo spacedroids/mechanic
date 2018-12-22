f48e4fedca32c764426866d4894d3c85a6d878f6
Ryan Nordman <spacerobots@gmail.com>
2018-12-21 23:32:48 -0800

    Click to add progress, replace each car with another which increases by 1 needed click to complete.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,100 @@
-
-
 //Internal vars
 let wallet = 0;
 let cars = ["carRed2_007.png", "carRed3_007.png", "carRed4_006.png", "carRed5_004.png", "carRed6_006.png"];
-let garage1Obj = {
-    status: "repairing",
+
+let car1Obj = {
+    garage: "",
+    progress: 0,
+    workNeeded: 1,
+    value: 100,
+    img: "carRed2_007.png",
+    reset: function() {
+        this.progress = 0;
+        this.workNeeded++;
+        this.value += 100;
+        this.img = getRandomElement(cars);
+        car1.style.left = "0px";
+        car1.style.top = "0px";
+    },
     move: function() {
-        if(this.status === "leaving") {
-            garage1.style.left = garage1.style.left - 10 + "px";
-            garage1.style.top = garage1.style.top - 10 + "px";
+        let left = parseInt( window.getComputedStyle(car1).getPropertyValue("left"));
+        let top = parseInt( window.getComputedStyle(car1).getPropertyValue("top"));
+        car1.style.left = left - 1 + "px";
+        car1.style.top = top + 1 + "px";
+        let offset = parseInt(window.getComputedStyle(garage1).getPropertyValue("left"));
+        if(left < -offset) {
+            this.garage.empty();
         }
-    }
+    },
+};
+
+let garage1Obj = {
+    car: car1Obj,
+    status: "empty",
+    progressNumerator: $('#garage1 #progress #numerator'),
+    progressDenomenator: $('#garage1 #progress #denomenator'),
+    update: function() {
+        if(this.status === "leaving") {
+            this.car.move();
+        }
+    },
+    redraw: function() {
+        this.progressNumerator.text(this.car.progress);
+        this.progressDenomenator.text(this.car.workNeeded);
+    },
+    reset: function() {
+        this.status = "repairing";
+    },
+    fix: function(unit) {
+        if(!this.car) {
+            return;
+        }
+        this.car.progress += unit;
+        if(this.car.progress >= this.car.workNeeded) {
+            wallet += this.car.value;
+            this.status = "leaving";
+        }
+    },
+    newCar: function(car) {
+        car.reset();
+        car.garage = this;
+        $('#car1').attr('src', 'img/' + car.img);
+    },
+    empty: function() {
+        this.status = "empty";
+        this.newCar(car1Obj);
+    },
 };
 
 //Display elements
 let walletEl = $('#wallet');
+let garage1El = $('#garage1');
 
 //Helper methods/game logic
-let newCustomer = function() {
-    $('#garage1 > img').attr('src', 'img/' + getRandomElement(cars));
-};
-
 let update = function() {
+    garage1Obj.update();
+}
+
+let redraw = function() {
     walletEl.text(wallet);
+    garage1Obj.redraw();
 };
 
-let mainClick = function() {
-    wallet++;
-    update();
+let newCustomer = function() {
+    garage1Obj.newCar(car1Obj.reset());
 };
 
 //Event handlers
-$('#garages').on('click', $('button'), mainClick);
-$('#newCar').click(newCustomer);
-driveaway.onmousedown = function() {
-    console.log('yesss');
-    garage1Obj.status = "leaving";
-}
+$('#car1').click(() => garage1Obj.fix(1));
+// driveaway.onmousedown = function() {
+//     garage1Obj.status = "leaving";
+// }
 
-//Tick
+//setup
+garage1Obj.newCar(car1Obj);
+
+
 window.setInterval(function(){
-    garage1Obj.move();
     update();
-}, 100);
+    redraw();
+}, 1);
