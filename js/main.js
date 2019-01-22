afe3b6384ff5bd9461945e73f7ed46d24f28cef2
Ryan Nordman <spacerobots@gmail.com>
2019-01-21 17:52:12 -0800

    Refactored several parts to be objects.

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ class Wallet {
     getBalance() {
         return this.funds;
     }
+    update() {}
     redraw() {
         this.$el.text(this.funds);
     }
@@ -41,62 +42,112 @@ class Car {
         this.value = value;
         this.img = getRandomElement(cars);
         this.id = uniqueId();
-        this.el$ = $('<img>').addClass('car').attr('id',uniqueId()).attr('src','img/'+this.img);
+        this.$el = $('<img>').addClass('car').attr('id',uniqueId()).attr('src','img/'+this.img);
     }
     //Animate driving this car away
     driveAway() {
-        this.el$.css('position', 'absolute');
-        let left = this.el$.offset().left;
-        this.el$.css('left', left);
-        let x = -this.el$.offset().left - this.el$.width();
-        let y = this.el$.offset().left;
-        this.el$.animate({left: x, top: y*2}, 1200, 'swing', () => { this.delete() });
+        this.$el.css('position', 'absolute');
+        let left = this.$el.offset().left;
+        this.$el.css('left', left);
+        let x = -this.$el.offset().left - this.$el.width();
+        let y = this.$el.offset().left;
+        this.$el.animate({left: x, top: y*2}, 1200, 'swing', () => { this.delete() });
     }
     //Return the HTML element that should represent this car
     get html() {
         console.log(this);
-        return this.el$;
+        return this.$el;
     }
     delete() {
-        this.el$.remove();
+        this.$el.remove();
     }
 }
 
 class Supply {
-    constructor(max, cost, wallet) {
-        this.amount = 0;
+    constructor(max, cost, wallet, amount) {
+        this.amount = amount;
         this.max = max;
         this.cost = cost;
         this.wallet = wallet;
     }
-    buy() {
-        
+    buy(units=1) {
+        console.log("you bought something");
     }
     add() {}
+    update() {}
+    redraw() {}
+    checkMax() {
+        return this.amount >= this.max;
+    }
 }
 
-let garage1Obj = {
-    el$: $('#garage1'),
-    car: null,
-    wallet: null,
-    status: "empty",
-    progressNumerator: $('#garage1 #progress #numerator'),
-    progressDenomenator: $('#garage1 #progress #denomenator'),
-    update: function() {
+class verticalSupply extends Supply {
+    constructor($parent, max, cost, wallet, amount=0) {
+        super(max, cost, wallet, amount);
+        this.$el = $(`
+        <div>
+        <div class="progress progress-bar-vertical">
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="height: 0%; background-color: black"></div>
+        </div>
+        <div>Oil</div>
+        </div>`);
+        $parent.append(this.$el);
+        this.$el.click(() => {
+            if(this.checkMax()) {
+                return;
+            }
+            this.buy(10);
+        });
+    }
+    buy(units=1) {
+        /* Check cost and deduct money */
+        if(this.wallet.withdraw(this.cost * units)) {
+            /* Adjust oil level of variable & progress bar UI */
+            this.amount += units;
+            this.setProgressBar(this.amount);
+        }
+    }
+    setProgressBar(newValue) {
+        this.$el.find('.progress-bar').css("height", `${newValue}%`);
+    };    
+    
+}
+
+class Garage {
+    constructor($parent, wallet) {
+        this.$el = $(`<div class="garage" id="garage1">
+        <div>
+           <span class="numerator">0</span>
+           <span class="divider">/</span>
+           <span class="denominator">10</span>
+        </div>
+     </div>`);
+        $parent.append(this.$el);
+        this.car = null;
+        this.wallet = wallet;
+        this.status = "empty";
+    }
+    update() {
         if(this.status === "leaving") {
         }
         else if(this.status === "empty") {
             this.newCar(new Car(getRandomInt(3)+1, 100));
             this.status = "repairing";
         }
-    },
-    redraw: function() {
+    }
+    redraw() {
         if(this.car) {
-            this.progressNumerator.text(this.car.progress);
-            this.progressDenomenator.text(this.car.workNeeded);    
+            this.$el.find('.numerator').text(this.car.progress);
+            this.$el.find('.denominator').text(this.car.workNeeded);
         }
-    },
-    fix: function(unit) {
+    }
+    newCar(car) {
+        this.car = car;
+        car.garage = this;
+        this.$el.prepend(car.$el);
+        car.$el.click(() => this.fix(1));
+    }
+    fix(unit) {
         if(!this.car) {
             return;
         }
@@ -107,21 +158,17 @@ let garage1Obj = {
             this.car.driveAway();
             this.status = "empty";
         }
-    },
-    newCar: function(car) {
-        this.car = car;
-        car.garage = this;
-        this.el$.prepend(car.el$);
-        car.el$.click(() => garage1Obj.fix(1));
-    },
-    empty: function() {
+    }
+    empty() {
         this.status = "empty";
-    },
-};
+    }
+}
 
 //Helper methods/game logic
-let update = function() {
-    garage1Obj.update();
+let update = function(objects) {
+    objects.forEach(function(obj) {
+        obj.update();
+    })
 }
 
 let redraw = function(objects) {
@@ -130,38 +177,19 @@ let redraw = function(objects) {
     })
 };
 
-let setProgressBar = function($progressBar, newValue) {
-    $progressBar.css("height", `${newValue}%`);
-};
-
-let oilLevel = 0;
-//Event handlers
-$('#oil').click(function() { 
-    const oilCost = 10;
-    /* Check cost and deduct money */
-    if(wallet >= oilCost) {
-        wallet -= oilCost;
-    } else {return;}
-    /* Adjust oil level of variable & progress bar UI */
-    //Loading bar for oil. Use .set() to set to value up to 100
-    oilLevel = Math.min(oilLevel + 10, 100);
-    let currentOil = oilLevel;//$('#oil .ldbar-label').text(); //this updates over many frames
-    console.log(this);
-    console.log(parseInt(currentOil));
-    setProgressBar($(this).find('.progress-bar'), parseInt(currentOil)+10);
-});
-
 //Document ready and main execution
 $(function() {
     let gameObjects = [];
     let wallet = new Wallet($('#header'));
+    let garage1 = new Garage($('#garages'), wallet);
+    let oilSupply = new verticalSupply($('#supplies'), 100, 1, wallet);
     gameObjects.push(wallet);
-    garage1Obj.wallet = wallet;
-    gameObjects.push(garage1Obj);
+    gameObjects.push(garage1);
+    gameObjects.push(oilSupply);
 
     //Game loop
     window.setInterval(function(){
-        update();
+        update(gameObjects);
         redraw(gameObjects);
     }, 1);
 });
